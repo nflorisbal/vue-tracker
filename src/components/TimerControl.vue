73a0ee74889c40091fbce600c11b8bef19740786
Nelson Florisbal <nelson.florisbal@dbccompany.com.br>
2022-12-13 11:11:09 -0300

    feat: componentizado o botao conforme aula 2

--- src/components/TimerControl.vue
+++ src/components/TimerControl.vue
@@ -1,26 +1,32 @@
 <template>
   <div class="is-flex is-align-items-center is-justify-content-space-between">
     <DisplayStopwatch :timeInSeconds="timeInSeconds" />
-    <button class="button" @click="startTask" :disabled="timerRunning">
-      <span class="icon"><span class="fas fa-play"></span></span>
-      <span>play</span>
-    </button>
-    <button class="button" @click="stopTask" :disabled="!timerRunning">
-      <span class="icon"><span class="fas fa-stop"></span></span>
-      <span>stop</span>
-    </button>
+    <TrackerButton
+      buttonText="play"
+      iconClass="fas fa-play"
+      :disableStatus="timerRunning"
+      @click="startTask"
+    />
+    <TrackerButton
+      buttonText="stop"
+      iconClass="fas fa-stop"
+      :disableStatus="!timerRunning"
+      @click="stopTask"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 import DisplayStopwatch from './DisplayStopwatch.vue';
+import TrackerButton from './TrackerButton.vue';
 
 export default defineComponent({
   name: 'TimerControl',
   emits: ['start', 'stop'],
   components: {
     DisplayStopwatch,
+    TrackerButton,
   },
   data() {
     return {
---
+++ src/components/TrackerButton.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="button" @click="clicked" :disabled="disableStatus">
+    <span class="icon"><span :class="iconClass"></span></span>
+    <span>{{ buttonText }}</span>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TrackerButton',
+  props: {
+    disableStatus: {
+      type: Boolean,
+      default: false,
+    },
+    buttonText: {
+      type: String,
+      required: true,
+    },
+    iconClass: {
+      type: String,
+      required: true,
+    },
+  },
+  methods: {
+    clicked(): void {
+      this.$emit('clicked');
+    },
+  },
+});
+</script>
